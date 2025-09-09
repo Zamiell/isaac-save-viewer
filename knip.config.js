29087cf184647dbfe13c707cf15dbe8e7099bf39
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-09-09 08:46:14 -0400

    update

--- knip.config.js
+++ knip.config.js
@@ -2,7 +2,23 @@
 
 /** @type {import("knip").KnipConfig} */
 const config = {
-  ignore: ["eslint.config.mjs", "prettier.config.mjs", "static/main.js"],
+  ignore: [
+    "eslint.config.mjs", // ESLint is provided by "complete-lint".
+    "prettier.config.mjs", // Prettier is provided by "complete-lint".
+    // @template-customization-start
+    "src/types/IsaacSaveFile.d.ts",
+    "src/types/KaitariStream.d.ts",
+    "static/lib/IsaacSaveFile.js",
+    "static/lib/KaitaiStream.js",
+    "static/main.js",
+    // @template-customization-end
+  ],
+  ignoreBinaries: [
+    "tsx", // This is provided by "complete-lint".
+  ],
+  ignoreDependencies: [
+    "complete-lint", // This is a linting meta-package.
+  ],
 };
 
 export default config;
